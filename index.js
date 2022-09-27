ea6ea5521385f55d0403165bef1f9fc2e989a12a
Victoria Le <leqvictoria@gmail.com>
2022-09-26 20:56:43 -0400

    theme toggler works

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
  const sideMenu = document.querySelector('aside')
  const menuBtn = document.querySelector('#menu-btn')
  const closeBtn = document.querySelector('#close-btn')
+ const themeToggler = document.querySelector('.theme-toggler')
 
-
+// Open Side Bar 
  menuBtn.addEventListener('click', () => {
    sideMenu.style.display = 'block'
  }
  )
-
+// Close Side Bar
  closeBtn.addEventListener('click', () => {
    sideMenu.style.display = 'none'
  }
  )
+// Change Theme 
+themeToggler.addEventListener('click', () => {
+  document.body.classList.toggle('dark-theme-variables')
+  themeToggler.querySelector('span:nth-child(1)').classList.toggle('active')
+  themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
+}
+)
