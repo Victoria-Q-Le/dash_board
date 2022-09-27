0b99d0d8719a912ce300dc4a16e69d0e961eea1b
Victoria Le <leqvictoria@gmail.com>
2022-09-27 17:07:54 -0400

    linked mocked data file

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders =[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        payment: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        payment: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko 11 Pro Drone',
+        productNumber: '49347',
+        payment: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '96996',
+        payment: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        payment: 'Paid',
+        shipping: 'En-route'
+    }
+]
